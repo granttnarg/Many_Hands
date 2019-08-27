12cd87dc674e0be52042bcd30355724688f821be
Grant Thomas <grantwilliamthomas@hotmail.co.uk>
2019-08-27 18:08:17 +0200

    done with seeds now have photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,21 +10,49 @@ puts "deleting db"
 Spot.destroy_all if Rails.env.development?
 Event.destroy_all
 User.destroy_all if Rails.env.development?
+Request.destroy_all if Rails.env.development?
+
+event_photo_one = "https://res.cloudinary.com/dqnhjr33l/image/upload/v1566918865/680w_x395h_c-reduced_JLS_rqnpkd.jpg"
+cover_photo_one = "https://res.cloudinary.com/dqnhjr33l/image/upload/v1566296013/xa811ufguq5mebljwl8p.jpg"
+profile_photo_one = "https://res.cloudinary.com/dqnhjr33l/image/upload/v1566303086/kfudn3dzyobcebdkauqs.jpg"
 
 user_one = User.new(first_name: "Pete", last_name: "Petey", email: "pete@example.com", password: "123456", is_creative?: false)
+user_one.remote_coverphoto_url = cover_photo_one
+user_one.remote_profile_photo_url = profile_photo_one
+
 event_one = Event.new(name: "The Big Party")
 event_one.user = user_one
+event_one.remote_photo_url = event_photo_one
 user_one.save!
 event_one.save!
 spot_one = Spot.new(role: "saxophone", description: "Looking for someone who can improvise", category: 1 )
 spot_one.event = event_one
 spot_one.save!
 
-user_two = User.new(first_name: "Jim", last_name: "Jimmy", email: "jim@example.com", password: "123456", is_creative?: false )
+
+
+
+event_photo_two = "https://res.cloudinary.com/dqnhjr33l/image/upload/v1566918874/anthony-delanoix-hzgs56Ze49s-unsplash_sq92xx.jpg"
+cover_photo_two = "https://res.cloudinary.com/dqnhjr33l/image/upload/v1565967626/photo-1512813195386-6cf811ad3542_n7awjf.jpg"
+profile_photo_two = "https://res.cloudinary.com/dqnhjr33l/image/upload/v1566303089/xrlmafij2gkxr6sfuqkc.jpg"
+
+
+user_two = User.new(first_name: "Jim", last_name: "Jimmy", email: "jim@example.com", password: "123456", is_creative?: false, coverphoto: "https://res.cloudinary.com/dqnhjr33l/image/upload/v1565967626/photo-1512813195386-6cf811ad3542_n7awjf.jpg", profile_photo: "https://res.cloudinary.com/dqnhjr33l/image/upload/v1566303089/xrlmafij2gkxr6sfuqkc.jpg" )
+user_two.remote_coverphoto_url = cover_photo_two
+user_two.remote_profile_photo_url = profile_photo_two
+
+
 event_two = Event.new(name: "Best Event Ever")
 event_two.user = user_two
+event_two.remote_photo_url = event_photo_two
+
+
+
+
 user_two.save!
 event_two.save!
+
+
 spot_two = Spot.new(role: "live painter", description: "Looking for someone to paint a cavans live to the rhythm of a band", category: 2 )
 spot_two.event = event_two
 spot_two.save!
@@ -32,16 +60,26 @@ spot_three = Spot.new(role: "singer", description: "Looking for a powerful rnb s
 spot_three.event = event_two
 spot_three.save!
 
-user_three = User.new(first_name: "Bill", last_name: "Billy", email: "bill@example.com", password: "123456", is_creative?: true )
+
+
+user_three = User.new(first_name: "Bill", last_name: "Billy", email: "bill@example.com", password: "123456", is_creative?: true)
+
+
+cover_photo_three = "https://res.cloudinary.com/dqnhjr33l/image/upload/v1566296014/q8jcbww960exrmyktpdq.jpg"
+profile_photo_three = "https://res.cloudinary.com/dqnhjr33l/image/upload/v1566555775/yerzqaags4tb59ltirsb.jpg"
+
+
+user_three.remote_coverphoto_url = cover_photo_three
+user_three.remote_profile_photo_url = profile_photo_three
+user_three.save!
 
 request_one = Request.new(status: 0, note: "Loved your painting, check out our event you would be perfect for it!")
-request_one.save!
 request_one.spot = spot_two
-request_one.user = user_three
+request_one.user_id = user_three.id
 request_one.save!
 
 
 
 
-puts "created seed, 1 user with 1 event and 1 spot &&&& 1 user with 1 event with 2 spots"
+puts "created seed, 1 user with 1 event and 1 spot &&&& 1 user with 1 event with 2 spots, 1 creative user, 1 request"
 
