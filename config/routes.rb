5dec205fa1138dfb744e516e1c804a14faed8efe
Jorge Calero <jorge.calero@gmail.com>
2019-08-27 15:58:19 +0200

    created requests controller, new/create methods in itand set the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-
   devise_for :users
   root to: 'pages#home'
 
   resources :events, only: [:new, :create, :show] do
     resources :spots, only: [:new, :create]
+  end
+
+  resources :spots, only: [:new, :create] do
+    resources :requests, only: [:new, :create]
   end
 end
