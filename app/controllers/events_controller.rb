6ae3cf2d22a0bc6bcf06a6caf017d3750e8016c5
Lestat McCree <lestatmccree@hotmail.com>
2019-08-27 17:33:35 +0200

    added alert and notice

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -39,7 +39,7 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
     authorize @event
     @event.destroy
-    redirect_to root_path
+    redirect_to root_path, notice: "Successfully deleted ;)"
   end
 
   private
