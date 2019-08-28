90f99c4f2943f4ebba3bd104ae4bfb6c5406212c
Kaylee Cho <kaylee.j.cho@gmail.com>
2019-08-28 11:35:37 +0200

    can sign up by giving your user type.and it leads to different index view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   include Pundit
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
   # Pundit: white-list approach.
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
@@ -13,6 +14,22 @@ class ApplicationController < ActionController::Base
   #   flash[:alert] = "You are not authorized to perform this action."
   #   redirect_to(root_path)
   # end
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:is_creative])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:is_creative])
+  end
+
+  def after_sign_in_path_for(resource)
+    if resource.is_creative?
+      events_path
+    else
+      events_path
+    end
+  end
 
   private
 
