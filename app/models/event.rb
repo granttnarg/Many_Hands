94b95da3aa7573d09cda8e45576ef5996cd864c9
Grant Thomas <grantwilliamthomas@hotmail.co.uk>
2019-08-30 14:04:08 +0200

    changed event index font bold

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,10 +2,10 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :spots
   validates :name, presence: true
-  validates :start_date, presence: true
-  validates :end_date, presence: true
-  validates :start_time, presence: true
-  validates :end_time, presence: true
+  #validates :start_date, presence: true
+  #validates :end_date, presence: true
+  #validates :start_time, presence: true
+  #validates :end_time, presence: true
 
   mount_uploader :photo, PhotoUploader
 
