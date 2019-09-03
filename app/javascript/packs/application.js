f553f2b56fdeaae428c42712c01855b600c559a5
Jorge Calero <jorge.calero@gmail.com>
2019-09-03 16:09:51 +0200

    added checkboxes to spot categories

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,22 @@ flatpickr(".datepicker", {
 	enableTime: true
 	
 });
+
+// $(document).ready(function(){
+//   $(".category-choice").click(function(){
+//     $(this).toggleClass("active");
+//   });
+// });
+
+
+
+const categoryBtns = document.querySelectorAll('.category-choice')
+categoryBtns.forEach((category) => {
+	category.addEventListener("click", (event) => {
+
+	});
+});
+
+
+
+console.log(categoryBtns);
