e69254312b82e46cf4c454cc29702253aab564fc
Lestat McCree <lestatmccree@hotmail.com>
2019-09-04 13:45:35 +0200

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2019_09_03_145003) do
-=======
 ActiveRecord::Schema.define(version: 2019_09_04_101300) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -121,10 +117,7 @@ ActiveRecord::Schema.define(version: 2019_09_04_101300) do
   add_foreign_key "events", "users"
   add_foreign_key "invitations", "spots"
   add_foreign_key "invitations", "users"
-<<<<<<< HEAD
   add_foreign_key "photos", "users"
-=======
   add_foreign_key "reviews", "requests"
->>>>>>> master
   add_foreign_key "spots", "events"
 end
